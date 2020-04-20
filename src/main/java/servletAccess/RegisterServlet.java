1d9944e4645684ffbe99c4462b50a473f2cc80fa
Federica Galbiati <galbiati.federica93@gmail.com>
2020-04-20 18:36:16 +0200

    courses,instructors, how to pages created

--- src/main/java/servletAccess/RegisterServlet.java
+++ src/main/java/servletAccess/RegisterServlet.java
@@ -1,6 +1,7 @@
 package servletAccess;
 
 import java.io.IOException;
+import java.sql.Date;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+
+import dao.UserDao;
+import javaBeans.User;
 
 @WebServlet("/htmlAccess/Register")
 public class RegisterServlet extends HttpServlet {
@@ -21,10 +25,23 @@ public class RegisterServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		String username = request.getParameter("usrn");
+		String userName = request.getParameter("usrn");
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String address = request.getParameter("address");
+		String birthDate = request.getParameter("birthDate");
 		String password = request.getParameter("pswd");
+		String password2 = request.getParameter("pswd2");
+		
+		UserDao dao = new UserDao(ds);
+		//devo mettere un if per controllare che non esista già userName
+		//se esiste già rimando a una pagina register.jsp in cui tengo salvati i dati inseriti a parte username e password
+		//se non esiste controllo che pass=pass2 e se sono uguali faccio setUser
+		
+//		User newUser = dao.setUser(birthDate, userName, firstName, lastName, email,
+//				phone, String address, password);
 	}
 	
 
