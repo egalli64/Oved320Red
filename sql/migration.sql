9c0ea5de5c67c89f3e0485c3ff20940b35c216fd
ReyPat94 <rey.paterlini@gmail.com>
2020-04-20 14:25:02 +0200

    modified migration

--- sql/migration.sql
+++ sql/migration.sql
@@ -1,5 +1,14 @@
-
+drop table if exists users_lessons;
+drop table if exists users_courses;
+drop table if exists lessons;
+drop table if exists staff;
+drop table if exists jobs;
+drop table if exists courses;
+drop table if exists categories;
 drop table if exists users;
+
+
+
 create table users (
 user_id integer primary key auto_increment,
 birth_date date,
@@ -13,8 +22,6 @@ med_certificate varchar(3),
 subscr_date date,
 passw varchar(15));
 
-delete from users
-where user_id = 1;
 
 insert into users (birth_date, user_name, first_name, last_name, e_mail, phone_number, address, med_certificate, subscr_date, passw) 
 values("1994/04/21", "Lory", "Lorenzo", "Cefala", "lorenzo.cefala@polimi.mail.it", "3381324433", "Milano, via Corso Como 23", "YES", "2019/04/13", "Sonoscemolol"),
@@ -24,7 +31,7 @@ values("1994/04/21", "Lory", "Lorenzo", "Cefala", "lorenzo.cefala@polimi.mail.it
 ("1997/03/28", "Rob Bolle", "Roberto", "Bolle", "lorenzo.cefala@hotmail.com", "338135763", "Milano, viale Bligny 11", "YES", "2019/04/13", "illan8894"),
 ("1999/02/24", "Rick", "Riccardo", "Melli", "Riccardo.melli@polimi.mail.it", "3357224433", "Milano, via Paolo Sarpi 5", "YES", "2019/04/13", "poptut1999");
 
-drop table if exists categories;
+
 create table categories (
 category_id varchar(5) primary key,
 category_name varchar(20));
@@ -35,7 +42,7 @@ values ("DNZ", "Danza"),
 ("AER", "Aerobica"),
 ("MART", "Arti marziali");
 
-drop table if exists courses;
+
 create table courses (
 course_id integer primary key auto_increment,
 course_name varchar(20),
@@ -52,7 +59,7 @@ values ("fondamenti di danza", "DNZ", 90),
 ("total body", "AER", 390),
 ("danza classica", "DNZ", 190);
 
-drop table if exists jobs;
+
 create table jobs (
 job_id varchar(10) primary key,
 job_title varchar(30),
@@ -66,7 +73,7 @@ values ("INST", "Instructor", "800", "2000"),
 ("ADV_INS", "Advanced Instructor", "800", "2000"),
 ("AD_Por", " Porter", "600", "1400");
 
-drop table if exists staff;
+
 create table staff (
 staff_id integer primary key auto_increment,
 birth_date date,
@@ -95,7 +102,7 @@ values("1994/04/21", "Sana", "Sara", "Naro", "sara.naro@polimi.mail.it", "338132
 ("1999/02/24", "Emma", "Emma", "Bondavalli", "emma.bondavalli@polimi.mail.it", "3357224433", "Milano, via Paolo Sarpi 5", "YES", "2019/04/13", "poptut1999", "IT60X0542811101000000123456", 3100, "ADV_INS");
 
 
-drop table if exists lessons;
+
 create table lessons (
 lesson_id integer primary key auto_increment,
 course_id integer,
@@ -116,7 +123,7 @@ values (1,80,3,"https://www.youtube.com/watch?v=OJoExsA7884&list=PLJcOFqZkViHOGn
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXX    MIDDLE TABS    XXXXXXXXXXXXXXXXXX
 
-drop table if exists users_courses;
+
 create table users_courses (
 user_id integer,
 course_id integer,
@@ -127,7 +134,7 @@ foreign key(user_id) references users(user_id));
 alter table users_courses
 add constraint primary key(user_id, course_id);
 
-drop table if exists users_lessons;
+
 create table users_lessons (
 user_id integer,
 lesson_id integer,
