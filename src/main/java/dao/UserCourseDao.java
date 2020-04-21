d5f1a2a9cf1ef72f896031af279820c200839f9f
FCRA <fcrargenziano@gmail.com>
2020-04-21 14:13:29 +0200

    Dao in progress

---
+++ src/main/java/dao/UserCourseDao.java
@@ -0,0 +1,89 @@
+package dao;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import javaBeans.User;
+import javaBeans.UserCourses;
+
+public class UserCourseDao implements Closeable {
+	private static final String GET_ALL = "SELECT user_id, course_id FROM users_courses";
+	private static final String GET_USER = "SELECT user_id, course_id FROM users_courses where user_id = ?";
+	private static final String SET_COURSE = "INSERT INTO users_courses (user_id, course_id) values(?, ?)";
+
+	private Connection conn;
+
+	public UserCourseDao(DataSource ds) {
+		try {
+			this.conn = ds.getConnection();
+		} catch (SQLException se) {
+			throw new IllegalStateException("Connection failed" + se.getMessage());
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		try {
+			conn.close();
+		} catch (SQLException se) {
+			throw new IllegalStateException("Failed to close connection" + se.getMessage());
+		}
+	}
+
+	// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX QUERY METHODS
+	// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+	public List<UserCourses> getAll() {
+		List<UserCourses> results = new ArrayList<>();
+
+		try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(GET_ALL)) {
+			while (rs.next()) {
+				UserCourses usercourses = new UserCourses(rs.getInt(1), rs.getInt(2));
+				results.add(usercourses);
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+		return results;
+	}
+
+	public List<UserCourses> getCoursesByUserID(int userID) {
+		List<UserCourses> results = new ArrayList<>();
+
+		try (PreparedStatement pstmt = conn.prepareStatement(GET_USER);) {
+			pstmt.setInt(1, userID);
+
+			try (ResultSet rs = pstmt.executeQuery();) {
+				while (rs.next()) {
+					results.add(new UserCourses(rs.getInt(1), rs.getInt(2)));
+				}
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+
+		}
+		return results;
+	}
+//
+//	@Resource(name = "jdbc/red")
+//	private DataSource ds;
+//
+////	public List<UserCourses> getCoursesByUsername(String username) {
+////		UserDao userdao = new UserDao(ds);
+////		int user_id = userdao.getUser(username).getUserID();
+////
+////	}
+////
+}
